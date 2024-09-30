4bd1063ac758837494863ec080a0eedd6993895b
Oscar Vargas <oscarirn09@gmail.com>
2024-09-30 16:07:56 -0500

    CAMBIOS EN ENTIDAD CUSTOMER

--- e-comerce/src/main/java/com/pedidos/e_comerce/customer/entity/CustomerEntity.java
+++ e-comerce/src/main/java/com/pedidos/e_comerce/customer/entity/CustomerEntity.java
@@ -1,5 +1,80 @@
 package com.pedidos.e_comerce.customer.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@lombok.Data
+@Entity
+@Table(name = "customer")
 public class CustomerEntity {
+	
+	private String id;
+	private String name;
+	private String email;
+	private String address;
+	private String phone;
+
+	public CustomerEntity() {
+		super();
+	}
+
+	public CustomerEntity(String id, String name, String email, String address, String phone) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.address = address;
+		this.phone = phone;
+	}
+
+	public CustomerEntity(String name, String email, String address, String phone) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.address = address;
+		this.phone = phone;
+	}
+// getters and setters
+	
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
 }
